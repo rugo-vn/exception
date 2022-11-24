830a5441a4f4e09647e11ee7355cfb275ce49021
Vu Anh Hao <hi@haova.me>
2022-11-24 16:27:52 +0700

    Add basic exception

---
+++ src/index.js
@@ -0,0 +1,32 @@
+export class RugoException extends Error {
+  constructor (message) {
+    super(message);
+
+    this.status = 400;
+    this.title = this.constructor.name;
+    this.detail = message || 'Something wrong.';
+  }
+}
+
+export class ValidationError extends RugoException {
+  constructor (msg) {
+    super(msg);
+
+    this.status = 400;
+  }
+}
+
+export const ajvError = function(err) {
+  switch (err.keyword) {
+    // non-nested
+    case 'required':
+      return new ValidationError(`Required value for properties "${err.params.missingProperty}"`);
+
+    case 'minimum':
+    case 'maximum':
+      return new ValidationError(`Value ${err.value} is out of ${err.keyword} range ${err.params.limit}`);
+
+    default:
+      return new ValidationError(`Document failed validation in operation "${err.keyword}"`);
+  }
+}
